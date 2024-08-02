f9e81e0441c25882b35c8ca252c4a0c1562b5e78
Nong AnuchitO <anuchito.code@gmail.com>
2024-08-02 18:02:58 +0700

    add metrics

--- README.md
+++ README.md
@@ -18,3 +18,26 @@ then
 ```bash
 make run-container
 ```
+
+
+
+# NOTE
+
+## Runtime Metrics
+## Alloc
+- Bytes of allocated heap objects. This represents the amount of memory currently in use by your Go program.
+
+## TotalAlloc
+- Cumulative bytes allocated for heap objects. This is a running total of all heap memory allocated since the program started, regardless of whether it's still in use. It will always increase, even if memory is freed.
+
+## Sys
+- Total bytes of memory obtained from the OS. This includes memory allocated for the heap, stacks, and other internal data structures. It's important to note that this doesn't necessarily represent the amount of physical memory used by the program.
+
+## HeapAlloc
+- Bytes of allocated heap objects. This is identical to Alloc. It's included for compatibility reasons.
+- Note: The NumGC field represents the number of garbage collection cycles completed. While it can indirectly indicate memory pressure, it's not a direct measure of memory usage.
+
+## Notes
+- Memory Units: The values are in bytes. You've converted them to megabytes (MB) for better readability.
+- Garbage Collection: The Go garbage collector reclaims unused memory, but the TotalAlloc value continues to increase.
+- OS Memory Management: The actual memory usage of your program might differ from the reported values due to operating system memory management techniques.
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 	"time"
 )
 
@@ -15,6 +17,18 @@ func l(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+type Size uint64
+
+const (
+	Byte Size = 1 << (10 * iota)
+	KB
+	MB
+)
+
+func toMB(b uint64) float64 {
+	return float64(b) / float64(MB)
+}
+
 func main() {
 	host, err := os.Hostname()
 	if err != nil {
@@ -22,6 +36,23 @@ func main() {
 	}
 	http.HandleFunc("/", l(func(w http.ResponseWriter, r *http.Request) {
 		w.Write(status("I am ok", host))
+	}))
+
+	http.HandleFunc("/metrics", l(func(w http.ResponseWriter, r *http.Request) {
+		var mem runtime.MemStats
+		runtime.ReadMemStats(&mem)
+		alloc := toMB(mem.Alloc)
+		totalAlloc := toMB(mem.TotalAlloc)
+		sysAlloc := toMB(mem.Sys)
+		heapInuse := toMB(mem.HeapInuse)
+		heapIdle := toMB(mem.HeapIdle)
+		heapReleased := toMB(mem.HeapReleased)
+		stackInuse := toMB(mem.StackInuse)
+		stackSys := toMB(mem.StackSys)
+
+		w.Header().Set("Content-Type", "application/json")
+		metrics := fmt.Sprintf(`{"size": "MB", "Alloc": %.2f, "TotalAlloc": %.2f, "Sys": %.2f, "HeapInuse": %.2f, "HeapIdle": %.2f, "HeapReleased": %.2f, "StackInuse": %.2f, "StackSys": %.2f}`, alloc, totalAlloc, sysAlloc, heapInuse, heapIdle, heapReleased, stackInuse, stackSys)
+		w.Write([]byte(metrics))
 	}))
 
 	http.HandleFunc("/healthz", l(func(w http.ResponseWriter, r *http.Request) {
@@ -42,7 +73,7 @@ func main() {
 		port = "8080"
 	}
 
-	log.Println("start server at port ", port)
+	log.Printf("start server at host %s port %s\n", host, port)
 	log.Fatal(http.ListenAndServe(host+":"+port, nil))
 }
 
