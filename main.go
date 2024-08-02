5ad4501bade0120f65d8422b4348692395e48d8c
Nong AnuchitO <anuchito.code@gmail.com>
2024-08-02 21:10:10 +0700

    extract function

--- main.go
+++ main.go
@@ -39,20 +39,8 @@ func main() {
 	}))
 
 	http.HandleFunc("/metrics", l(func(w http.ResponseWriter, r *http.Request) {
-		var mem runtime.MemStats
-		runtime.ReadMemStats(&mem)
-		alloc := toMB(mem.Alloc)
-		totalAlloc := toMB(mem.TotalAlloc)
-		sysAlloc := toMB(mem.Sys)
-		heapInuse := toMB(mem.HeapInuse)
-		heapIdle := toMB(mem.HeapIdle)
-		heapReleased := toMB(mem.HeapReleased)
-		stackInuse := toMB(mem.StackInuse)
-		stackSys := toMB(mem.StackSys)
-
 		w.Header().Set("Content-Type", "application/json")
-		metrics := fmt.Sprintf(`{"size": "MB", "Alloc": %.2f, "TotalAlloc": %.2f, "Sys": %.2f, "HeapInuse": %.2f, "HeapIdle": %.2f, "HeapReleased": %.2f, "StackInuse": %.2f, "StackSys": %.2f}`, alloc, totalAlloc, sysAlloc, heapInuse, heapIdle, heapReleased, stackInuse, stackSys)
-		w.Write([]byte(metrics))
+		w.Write([]byte(metrics()))
 	}))
 
 	http.HandleFunc("/healthz", l(func(w http.ResponseWriter, r *http.Request) {
@@ -79,4 +67,18 @@ func main() {
 
 func status(s string, h string) []byte {
 	return []byte(`{"status": "` + s + `", "server": "` + h + `"}`)
+}
+
+func metrics() string {
+	var mem runtime.MemStats
+	runtime.ReadMemStats(&mem)
+	alloc := toMB(mem.Alloc)
+	totalAlloc := toMB(mem.TotalAlloc)
+	sysAlloc := toMB(mem.Sys)
+	heapInuse := toMB(mem.HeapInuse)
+	heapIdle := toMB(mem.HeapIdle)
+	heapReleased := toMB(mem.HeapReleased)
+	stackInuse := toMB(mem.StackInuse)
+	stackSys := toMB(mem.StackSys)
+	return fmt.Sprintf(`{"size": "MB", "Alloc": %.2f, "TotalAlloc": %.2f, "Sys": %.2f, "HeapInuse": %.2f, "HeapIdle": %.2f, "HeapReleased": %.2f, "StackInuse": %.2f, "StackSys": %.2f}`, alloc, totalAlloc, sysAlloc, heapInuse, heapIdle, heapReleased, stackInuse, stackSys)
 }
